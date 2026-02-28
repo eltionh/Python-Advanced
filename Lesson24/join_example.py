2da4b2ad4d78f07f17ae4bae24d8ebb2dc4d7d39
unknown <Student@DIGITALSCHOOL.LOCAL>
2026-02-28 05:47:22 -0800

    joins

---
+++ Lesson24/join_example.py
@@ -0,0 +1,78 @@
+import sqlite3
+
+conn = sqlite3.connect('example.db')
+cursor = conn.cursor()
+
+cursor.execute('''
+    create table if not exists students (
+         student_id integer primary key,
+         name text
+         
+     )
+
+''')
+
+cursor.execute('''
+    create table if not exists courses (
+         course_id integer primary key,
+         course_name text,
+         student_id integer,
+         foreign key (student_id) references students(student_id)
+
+     )
+
+''')
+
+cursor.execute("insert into students (name) values ('Alice')")
+cursor.execute("insert into students (name) values ('Bob')")
+
+
+cursor.execute("insert into courses (course_name) values ('Math', 1)")
+cursor.execute("insert into courses (course_name) values ('Science',1)")
+cursor.execute("insert into courses (course_name) values ('Art', 2)")
+
+
+conn.commit()
+
+
+# cursor.execute('''
+#         select students.name, courses.course_name
+#         from students
+#         join courses on students.student_id = courses.student_id
+# ''')
+
+#cursor.execute('''
+  #       select students.name, courses.course_name
+  #       from students
+ #        Inner join courses on students.student_id = courses.student_id
+ #''')
+
+#cursor.execute('''
+#         select students.name, courses.course_name
+#        from students
+#         Inner join courses on students.student_id = courses.student_id
+# ''')
+
+cursor.execute('''
+         select students.name, courses.course_name
+         from students
+         Left join courses on students.student_id = courses.student_id
+ ''')
+
+rows = cursor.fetchall()
+for row in rows:
+    print(f"Student: {row[0]}, Course: {row[1]}")
+
+conn.close()
+
+
+
+
+
+
+
+
+
+
+
+
